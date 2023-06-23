188908d6ef319f5774320af727544f4d931b2a5e
Anas Khan <anasalimkhan@gmail.com>
2023-06-23 10:19:43 +0530

    converting from js -> ts + adding support for publishBatch

---
+++ lib/index.d.ts
@@ -0,0 +1,81 @@
+/// <reference types="node" />
+import AWS, { AWSError } from "aws-sdk";
+import EventEmitter from "events";
+import { QueueAttributeMap, SendMessageBatchResult, SendMessageResult } from "aws-sdk/clients/sqs";
+import { PromiseResult } from "aws-sdk/lib/request";
+export default class SQS {
+    private name;
+    private emitter;
+    private config;
+    private client;
+    private queues;
+    constructor(name: string, emitter: EventEmitter, config?: {});
+    log(message: string, data?: Record<string, any>): void;
+    success(message: string, data?: Record<string, any>): void;
+    error(err: Error, data?: Record<string, any>): void;
+    init(): Promise<SQS>;
+    /**
+     * Create a new queue
+     * @param  {String} name
+     * @param  {Object} opts
+     * @param {Integer} opts.DelaySeconds=0 [The length of time, in seconds,
+     *                                      for which the delivery of
+     *                                      all messages in the queue is delayed]
+     * @param {Integer} opts.MaximumMessageSize=262,144 [Defaults to 262,144 (256 KiB)]
+     * @param {Integer} opts.MessageRetentionPeriod=345,600 [in seconds]
+     * @param {Integer} opts.ReceiveMessageWaitTimeSeconds=0 [in seconds, The length of time
+     *                                                       in seconds, for which a ReceiveMessage
+     *                                                        action waits for a message to arrive]
+     * @param {String} opts.FifoQueue='false' [Use FIFO (true) or Standard queue (false)]
+     * @return {Promise}
+     */
+    createQueue(name: string, opts?: QueueAttributeMap): Promise<void>;
+    /**
+     * Publish on SQS
+     * @param  {string}  name
+     * @param  {Object}  content
+     * @param  {Object}  meta={}
+     * @param  {Boolean} handle=true  [Should the error be handled]
+     * @param  {Object}  [options={}]
+     * @param  {Number}  [options.delay]  [in seconds]
+     * @return {Promise}
+     */
+    publish(name: string, content: Record<string, any>, meta?: Record<string, any>, handle?: boolean, options?: Record<string, any>): Promise<PromiseResult<SendMessageResult, AWSError>>;
+    /**
+     * Publish on SQS in batch
+     * @param  {string}  name
+     * @param  {Object[]}  contentList
+     * @param  {Object}  meta={}
+     * @param  {Boolean} handle=true  [Should the error be handled]
+     * @param  {Object}  [options={}]
+     * @param  {Number}  [options.delay]  [in seconds]
+     * @return {Promise}
+     */
+    publishBatch(name: string, contentList: Record<string, any>[], meta?: Record<string, any>, handle?: boolean, options?: Record<string, any>): Promise<PromiseResult<SendMessageBatchResult, AWSError>>;
+    publishFifo(name: string, content: Record<string, any>, meta: Record<string, any>, group: Record<string, any>, handle?: boolean): Promise<PromiseResult<SendMessageResult, AWSError>>;
+    getQueueUrl(name: string): string;
+    /**
+     * Subscribe to a queue, using long polling
+     */
+    subscribe(name: string, cb: (arg1: Record<string, any>, arg2: Record<string, any>) => void, opts?: Record<string, any>): Promise<unknown>;
+    deleteMessage(name: string, messageId: any, handle: string): Promise<void>;
+    returnMessage(name: string, messageId: any, handle: string): Promise<void>;
+    fetchMessages(name: string, number?: number): Promise<({
+        data: any;
+        ack: () => Promise<void>;
+        nack: () => Promise<void>;
+        id?: undefined;
+        handle?: undefined;
+        queueAttributes?: undefined;
+        messageAttributes?: undefined;
+    } | {
+        id: string;
+        handle: string;
+        queueAttributes: AWS.SQS.MessageSystemAttributeMap;
+        messageAttributes: AWS.SQS.MessageBodyAttributeMap;
+        data?: undefined;
+        ack?: undefined;
+        nack?: undefined;
+    })[][]>;
+    fetchOne(name: string): Promise<any>;
+}
---
+++ src/index.ts
@@ -0,0 +1,398 @@
+import AWS, { AWSError } from "aws-sdk";
+import safeJSON from "safely-parse-json";
+import { Consumer } from "sqs-consumer";
+import EventEmitter from "events";
+import {
+  QueueAttributeMap,
+  SendMessageBatchResult,
+  SendMessageRequest,
+  SendMessageResult,
+} from "aws-sdk/clients/sqs";
+import { PromiseResult } from "aws-sdk/lib/request";
+import {
+  ChangeMessageVisibilityRequest,
+  DeleteMessageRequest,
+  SendMessageBatchRequest,
+} from "@aws-sdk/client-sqs";
+import uuid from "uuid";
+
+export default class SQS {
+  private name: string;
+  private emitter: EventEmitter;
+  private config: Record<string, any>;
+  private client: AWS.SQS;
+  private queues: Record<string, string>;
+
+  constructor(name: string, emitter: EventEmitter, config = {}) {
+    this.name = name;
+    this.emitter = emitter;
+    this.config = Object.assign(
+      {
+        region: "us-east-1",
+      },
+      config
+    );
+    this.client = null;
+    this.queues = {};
+  }
+
+  log(message: string, data?: Record<string, any>) {
+    this.emitter.emit("log", {
+      service: this.name,
+      message,
+      data,
+    });
+  }
+
+  success(message: string, data?: Record<string, any>) {
+    this.emitter.emit("success", {
+      service: this.name,
+      message,
+      data,
+    });
+  }
+
+  error(err: Error, data?: Record<string, any>) {
+    this.emitter.emit("error", {
+      service: this.name,
+      data,
+      err,
+    });
+  }
+
+  async init(): Promise<SQS> {
+    this.client = new AWS.SQS(this.config);
+    // try to list queues
+    try {
+      const response = await this.client
+        .listQueues({
+          QueueNamePrefix: "",
+        })
+        .promise();
+      this.log(`Connected on SQS:${this.name}`, this.config);
+      const queueUrls = response.QueueUrls || [];
+      queueUrls.forEach((queueUrl) => {
+        const queueUrlSplits = queueUrl.split("/");
+        const queueName = queueUrlSplits[queueUrlSplits.length - 1];
+        this.queues = Object.assign(this.queues, {
+          [queueName]: queueUrl,
+        });
+      });
+    } catch (err) {
+      this.error(err, this.config);
+      throw err;
+    }
+    return this;
+  }
+
+  /**
+   * Create a new queue
+   * @param  {String} name
+   * @param  {Object} opts
+   * @param {Integer} opts.DelaySeconds=0 [The length of time, in seconds,
+   *                                      for which the delivery of
+   *                                      all messages in the queue is delayed]
+   * @param {Integer} opts.MaximumMessageSize=262,144 [Defaults to 262,144 (256 KiB)]
+   * @param {Integer} opts.MessageRetentionPeriod=345,600 [in seconds]
+   * @param {Integer} opts.ReceiveMessageWaitTimeSeconds=0 [in seconds, The length of time
+   *                                                       in seconds, for which a ReceiveMessage
+   *                                                        action waits for a message to arrive]
+   * @param {String} opts.FifoQueue='false' [Use FIFO (true) or Standard queue (false)]
+   * @return {Promise}
+   */
+  async createQueue(name: string, opts: QueueAttributeMap = {}): Promise<void> {
+    if (this.queues[name]) {
+      const queueUrl = this.queues[name];
+      const message = `Queue ${name} exists => ${queueUrl}`;
+      this.log(message, {
+        name,
+        queueUrl,
+      });
+      return Promise.resolve();
+    }
+    const options = Object.assign(
+      {
+        // FifoQueue: 'false', // use standard by default
+      },
+      opts
+    );
+    try {
+      const response = await this.client
+        .createQueue({
+          QueueName: name,
+          Attributes: options,
+        })
+        .promise();
+      const queueUrl = response.QueueUrl;
+      const message = `Created queue ${name} => ${queueUrl}`;
+      this.queues = Object.assign(this.queues, {
+        [name]: queueUrl,
+      });
+      this.log(message, { name, queueUrl });
+    } catch (err) {
+      this.error(err, { name, options });
+      throw err;
+    }
+  }
+
+  /**
+   * Publish on SQS
+   * @param  {string}  name
+   * @param  {Object}  content
+   * @param  {Object}  meta={}
+   * @param  {Boolean} handle=true  [Should the error be handled]
+   * @param  {Object}  [options={}]
+   * @param  {Number}  [options.delay]  [in seconds]
+   * @return {Promise}
+   */
+  async publish(
+    name: string,
+    content: Record<string, any>,
+    meta: Record<string, any> = {},
+    handle: boolean = true,
+    options: Record<string, any> = {}
+  ): Promise<PromiseResult<SendMessageResult, AWSError>> {
+    const queueUrl = this.queues[name];
+    if (!queueUrl) {
+      const error = new Error(`Queue ${name} does not exists`);
+      this.error(error, error.cause);
+      if (handle === false) {
+        return Promise.reject(error);
+      }
+    }
+    const params: SendMessageRequest = {
+      QueueUrl: this.queues[name],
+      MessageBody: JSON.stringify({ content, meta }),
+    };
+
+    if (typeof options.delay === "number") {
+      params.DelaySeconds = options.delay;
+    }
+
+    try {
+      const res = await this.client.sendMessage(params).promise();
+      return res;
+    } catch (err) {
+      this.error(err, {
+        queueName: name,
+        content,
+        meta,
+        handle,
+      });
+      if (handle === false) {
+        throw err;
+      }
+    }
+  }
+
+  /**
+   * Publish on SQS in batch
+   * @param  {string}  name
+   * @param  {Object[]}  contentList
+   * @param  {Object}  meta={}
+   * @param  {Boolean} handle=true  [Should the error be handled]
+   * @param  {Object}  [options={}]
+   * @param  {Number}  [options.delay]  [in seconds]
+   * @return {Promise}
+   */
+  async publishBatch(
+    name: string,
+    contentList: Record<string, any>[],
+    meta: Record<string, any> = {},
+    handle: boolean = true,
+    options: Record<string, any> = {}
+  ): Promise<PromiseResult<SendMessageBatchResult, AWSError>> {
+    const queueUrl = this.queues[name];
+    if (!queueUrl) {
+      const error = new Error(`Queue ${name} does not exists`);
+      this.error(error, error.cause);
+      if (handle === false) {
+        return Promise.reject(error);
+      }
+    }
+    let DelaySeconds: number | undefined;
+    if (typeof options.delay === "number") {
+      DelaySeconds = options.delay;
+    }
+
+    const params: SendMessageBatchRequest = {
+      QueueUrl: this.queues[name],
+      Entries: contentList.map((content) => ({
+        Id: uuid.v4(),
+        MessageBody: JSON.stringify({ content, meta }),
+        DelaySeconds,
+      })),
+    };
+
+    try {
+      const res = await this.client.sendMessageBatch(params).promise();
+      return res;
+    } catch (err) {
+      this.error(err, {
+        queueName: name,
+        contentList,
+        meta,
+        handle,
+      });
+      if (handle === false) {
+        throw err;
+      }
+    }
+  }
+
+  async publishFifo(
+    name: string,
+    content: Record<string, any>,
+    meta: Record<string, any> = {},
+    group: Record<string, any>,
+    handle: boolean = true
+  ): Promise<PromiseResult<SendMessageResult, AWSError>> {
+    const queueUrl = this.queues[name];
+    if (!queueUrl) {
+      const error = new Error(`Queue ${name} does not exists`);
+      this.error(error, error.cause);
+      if (handle === false) {
+        return Promise.reject(error);
+      }
+    }
+    const params: SendMessageRequest = {
+      QueueUrl: this.queues[name],
+      MessageBody: JSON.stringify({ content, meta }),
+      MessageGroupId: group.name,
+      MessageDeduplicationId: group.id,
+    };
+    try {
+      const res = await this.client.sendMessage(params).promise();
+      return res;
+    } catch (err) {
+      this.error(err, {
+        queueName: name,
+        content,
+        meta,
+        handle,
+      });
+      if (handle === false) {
+        throw err;
+      }
+    }
+  }
+
+  getQueueUrl(name: string): string {
+    const queueUrl = this.queues[name];
+    if (!queueUrl) {
+      const error = new Error(`Queue ${name} does not exists`);
+      this.error(error, error.cause);
+      throw error;
+    }
+    return queueUrl;
+  }
+
+  /**
+   * Subscribe to a queue, using long polling
+   */
+  subscribe(
+    name: string,
+    cb: (arg1: Record<string, any>, arg2: Record<string, any>) => void,
+    opts: Record<string, any> = {}
+  ) {
+    const queueUrl = this.getQueueUrl(name);
+    return new Promise((resolve) => {
+      const sub = Consumer.create({
+        queueUrl,
+        handleMessage: (msg) => {
+          return new Promise((_resolve, reject) => {
+            cb(
+              {
+                data: safeJSON(msg.Body),
+                ack: _resolve,
+                nack: (err) => {
+                  reject(err || new Error("Unable to process message"));
+                },
+              },
+              {
+                id: msg.MessageId,
+                handle: msg.ReceiptHandle,
+                queueAttributes: msg.Attributes,
+                messageAttributes: msg.MessageAttributes,
+              }
+            );
+          });
+        },
+        batchSize: opts.maxInProgress || 10,
+        // @ts-ignore
+        sqs: this.client,
+      });
+
+      sub.on("error", (err) => {
+        this.error(err, {
+          queueName: name,
+        });
+      });
+      sub.on("processing_error", (err) => {
+        this.error(err, {
+          queueName: name,
+        });
+      });
+      this.success(`Subscribed to ${queueUrl}`);
+      sub.start();
+      resolve(sub);
+    });
+  }
+
+  async deleteMessage(
+    name: string,
+    messageId: any,
+    handle: string
+  ): Promise<void> {
+    const queueUrl = this.getQueueUrl(name);
+    const params: DeleteMessageRequest = {
+      QueueUrl: queueUrl,
+      ReceiptHandle: handle,
+    };
+
+    await this.client.deleteMessage(params).promise();
+  }
+
+  async returnMessage(name: string, messageId: any, handle: string) {
+    const queueUrl = this.getQueueUrl(name);
+    const params: ChangeMessageVisibilityRequest = {
+      QueueUrl: queueUrl,
+      ReceiptHandle: handle,
+      VisibilityTimeout: 0,
+    };
+    await this.client.changeMessageVisibility(params).promise();
+  }
+
+  async fetchMessages(name: string, number = 10) {
+    const queueUrl = this.getQueueUrl(name);
+    const params = {
+      QueueUrl: queueUrl,
+      MaxNumberOfMessages: number,
+    };
+    const res = await this.client.receiveMessage(params).promise();
+    return res.Messages.map((msg) => {
+      return [
+        {
+          data: safeJSON(msg.Body),
+          ack: () => {
+            return this.deleteMessage(name, msg.MessageId, msg.ReceiptHandle);
+          },
+          nack: () => {
+            return this.returnMessage(name, msg.MessageId, msg.ReceiptHandle);
+          },
+        },
+        {
+          id: msg.MessageId,
+          handle: msg.ReceiptHandle,
+          queueAttributes: msg.Attributes,
+          messageAttributes: msg.MessageAttributes,
+        },
+      ];
+    });
+  }
+
+  async fetchOne(name: string): Promise<any> {
+    const messages = await this.fetchMessages(name, 1);
+    return messages[0];
+  }
+}
